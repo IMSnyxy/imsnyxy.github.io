8066c5f0f8509de347715fa303a937c01021ecd7
IMSnyxy <modeinsane123@gmail.com>
2023-07-04 00:34:23 +0800

    6

--- home.js
+++ home.js
@@ -31,25 +31,7 @@ function isElementInViewport(element) {
   );
 }
 
-// Add "show" class to the sections when they are in the viewport
-function showSections() {
-  var firstSection = document.querySelector('.first-section');
-  var secondSection = document.querySelector('.second-section');
 
-  if (isElementInViewport(firstSection)) {
-    firstSection.classList.add('show');
-  }
-
-  if (isElementInViewport(secondSection)) {
-    secondSection.classList.add('show');
-  }
-}
-
-// Listen for scroll event and show the sections
-window.addEventListener('scroll', showSections);
-
-// Show the sections initially if they are in the viewport on page load
-window.addEventListener('load', showSections);
 
 var cardTexts = document.getElementsByClassName('card-text-truncate');
 for (var i = 0; i < cardTexts.length; i++) {
@@ -59,3 +41,39 @@ for (var i = 0; i < cardTexts.length; i++) {
 $(document).ready(function() {
   $('.dropdown-toggle').dropdown();
 });
+
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to handle scrolling and add animation classes to sections
+function handleScroll() {
+  var firstSection = document.querySelector('.first-section');
+  var secondSection = document.querySelector('.second-section');
+  var thirdSection = document.querySelector('.third-section');
+
+  if (isInViewport(firstSection)) {
+    firstSection.classList.add('slide-up');
+  }
+
+  if (isInViewport(secondSection)) {
+    secondSection.classList.add('slide-up');
+  }
+
+  if (isInViewport(thirdSection)) {
+    thirdSection.classList.add('slide-up');
+  }
+}
+
+// Event listener for scroll event
+window.addEventListener('scroll', handleScroll);
+
+// Initial check on page load
+window.addEventListener('DOMContentLoaded', handleScroll);
