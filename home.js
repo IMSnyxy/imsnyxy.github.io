80fc4b89b3578bb7f9305110386c01a4892bf12b
IMSnyxy <modeinsane123@gmail.com>
2023-07-04 01:36:41 +0800

    FINAL 2

--- home.js
+++ home.js
@@ -20,18 +20,6 @@ function changeBackgroundImage() {
 // Start the slideshow
 setInterval(changeBackgroundImage, slideshowInterval);
 
-// Check if the element is in the viewport
-function isElementInViewport(element) {
-  var rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-
 
 var cardTexts = document.getElementsByClassName('card-text-truncate');
 for (var i = 0; i < cardTexts.length; i++) {
@@ -42,8 +30,8 @@ $(document).ready(function() {
   $('.dropdown-toggle').dropdown();
 });
 
-// Function to check if an element is in the viewport
-function isInViewport(element) {
+// Add the following JavaScript code
+function isElementInViewport(element) {
   var rect = element.getBoundingClientRect();
   return (
     rect.top >= 0 &&
@@ -53,27 +41,20 @@ function isInViewport(element) {
   );
 }
 
-// Function to handle scrolling and add animation classes to sections
 function handleScroll() {
-  var firstSection = document.querySelector('.first-section');
-  var secondSection = document.querySelector('.second-section');
-  var thirdSection = document.querySelector('.third-section');
+  var windowHeight = $(window).height();
+  var scrollTop = $(window).scrollTop();
 
-  if (isInViewport(firstSection)) {
-    firstSection.classList.add('slide-up');
-  }
-
-  if (isInViewport(secondSection)) {
-    secondSection.classList.add('slide-up');
-  }
-
-  if (isInViewport(thirdSection)) {
-    thirdSection.classList.add('slide-up');
-  }
+  $(".slide-up").each(function() {
+    var elementOffset = $(this).offset().top;
+    
+    if (scrollTop > elementOffset - windowHeight + 200) {
+      $(this).addClass("active");
+    }
+  });
 }
 
-// Event listener for scroll event
-window.addEventListener('scroll', handleScroll);
+$(window).on("scroll", handleScroll);
 
-// Initial check on page load
-window.addEventListener('DOMContentLoaded', handleScroll);
+// Initial scroll check on page load
+handleScroll();
